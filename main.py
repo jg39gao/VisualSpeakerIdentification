58a5c3063d8a4a7d5e87290d224d536630d337d3
BenMouncer <63462139+BenMouncer@users.noreply.github.com>
2020-04-27 18:14:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,32 +7,27 @@ Created on Tue Apr 21 11:21:24 2020
 Description - Code to link all the scripts of the project together
 """
 
-# BM TODO:
-# Test code works with full dataset
-# Import the face recognition components
-# Integrate the models and face recognition
+# TODO
+# Modify models to take test train and not split internally
+# Clean up CNN and PCA and get them working
 
-import clean
-import video2array
+import pic2array
 import model
 
 def main():
     
-    data_folder = "\data"
+    data = "\data_test_train_dev\\train"
+    # Beware this will eat memory like Google Chrome
+    # X Array Takes Up 13GB for the full dataset
     
-    # Clean the Text Files
-    clean.all_txt(data_folder)
-    
-    # Find the Raw Training/Test Data and store in np array
-    # of shape [No. pictures, 224 * 224 * 3]
-    X, Y = video2array.dataset(data_folder, exclude = {"lips-speaker","lips"})   
-    
+    X, Y = pic2array.faces2array(data)
+
     # Use face detection on a new video, extract images of faces as np arrays
     model.basic(X, Y)
     model.pca(X, Y)
     model.cnn(X, Y)
 
-    # Apply the trained model to the new images of faces
+    # Apply the trained model to the new images of faces (TODO)
 
 
 if __name__ == "__main__":
