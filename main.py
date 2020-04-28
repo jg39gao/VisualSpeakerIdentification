c535153f1f3ef44dff77ab6912922da70e2dec91
BenMouncer <63462139+BenMouncer@users.noreply.github.com>
2020-04-28 12:08:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,24 +8,25 @@ Description - Code to link all the scripts of the project together
 """
 
 # TODO
-# Modify models to take test train and not split internally
-# Clean up CNN and PCA and get them working
+# Apply the trained models to the new images of faces
 
 import pic2array
 import model
 
 def main():
     
-    data = "\data_test_train_dev\\train"
+    data_train = "\data_test_train_dev\\train"
+    data_test = "\data_test_train_dev\\test"
+    
     # Beware this will eat memory like Google Chrome
     # X Array Takes Up 13GB for the full dataset
-    
-    X, Y = pic2array.faces2array(data)
+    X_train, Y_train = pic2array.faces2array_sample(data_train)
+    X_test, Y_test = pic2array.faces2array_sample(data_test)
 
     # Use face detection on a new video, extract images of faces as np arrays
-    model.basic(X, Y)
-    model.pca(X, Y)
-    model.cnn(X, Y)
+    model.basic(X_train, X_test, Y_train, Y_test)
+    model.pca(X_train, X_test, Y_train, Y_test)
+    model.cnn(X_train, X_test, Y_train, Y_test)
 
     # Apply the trained model to the new images of faces (TODO)
 
